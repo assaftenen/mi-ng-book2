8bb4cbb30b4811cdb86c3276ca149d17b6bdb1d4
Lluis Toyos <toyos@toyos-PORTEGE-M700>
2016-12-27 11:17:00 +0100

    Incluido ProductsList

--- inventory-app/src/app/app.component.ts
+++ inventory-app/src/app/app.component.ts
@@ -6,6 +6,32 @@ import { Product } from './product/model/product.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
   products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/resources/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/resources/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product);
+  }
 }
--- inventory-app/src/app/product/products-list/products-list.component.ts
+++ inventory-app/src/app/product/products-list/products-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,EventEmitter } from '@angular/core';
+import { Product } from '../model/product.model';
 @Component({
   selector: 'products-list',
+  inputs: ['productList'],
+  outputs: ['onProductSelected'],
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
+  /**
+   * @input productList - the Product[] passed to us
+   */
+  productList: Product[];
 
-  constructor() { }
+  /**
+   * @output onProductSelected - outputs the current
+   *          Product whenever a new Product is selected
+   */
+  onProductSelected: EventEmitter<Product>;
 
-  ngOnInit() {
+  /**
+   * @property currentProduct - local state containing
+   *             the currently selected `Product`
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
   }
 
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+  ngOnInit(){
+
+  }
 }
